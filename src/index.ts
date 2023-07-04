6359e02da4c72386f13d500aa7134f313e46d407
Maxino22 <ajaybullec@gmail.com>
2023-07-04 13:40:11 +0300

    added single query

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,22 @@ const resolvers = {
 		games() {
 			return _db.games
 		},
+
+		game(_, args) {
+			return _db.games.find((game) => game.id === args.id)
+		},
 		reviews() {
 			return _db.reviews
 		},
+		review(_, args) {
+			return _db.reviews.find((review) => review.id === args.id)
+		},
 		authors() {
 			return _db.authors
+		},
+
+		author(_, args) {
+			return _db.authors.find((author) => author.id === args.id)
 		},
 	},
 }
